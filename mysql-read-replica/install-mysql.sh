fb1d9d5721da386b78d7b37e921c2c09cee46808
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-24 11:53:20 -0300

    .

--- mysql-read-replica/install-mysql.sh
+++ mysql-read-replica/install-mysql.sh
@@ -1,6 +1,13 @@
 # update OS
 sudo yum update -y 
 
+ok=$(rpm -qa|grep mysql-community-server |wc -l |xargs)
+
+if [ $ok -gt 0 ] ; then
+    echo "Mysql already installed. Nothing to do"
+	exit 0
+fi
+
 # enable mysql repo
 wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm --no-check-certificate
 sudo rpm -ivh mysql57-community-release-el7-11.noarch.rpm
@@ -9,7 +16,29 @@ sudo yum update -y
 
 sudo yum install mysql-server -y
 
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
 
-#wget https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl
-#perl ./mysqltuner.pl
+mysqlPwd=$(grep 'A temporary password is generated for' /var/log/mysqld.log |awk '{print $NF}')
 
+if [ -n "$1" ] ; then
+    newpw="$1"
+else
+    newpw="AzureRocks100!"
+fi
+
+echo "Setting new password: $newpw"
+
+if [ ! -e ~/.my.cnf ] ; then
+    echo "
+[client]
+user=root
+password=$mysqlPwd
+host=localhost" > ~/.my.cnf
+
+mysqladmin -u root password $newpw
+else
+    echo "~/.my.cnf already there - nothing to do"
+fi
+
+exit 0
