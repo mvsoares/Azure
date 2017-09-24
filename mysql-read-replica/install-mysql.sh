014edbb7f8f84a51177042fed7591f13ecb36e74
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-24 10:59:12 -0300

    .

--- mysql-read-replica/install-mysql.sh
+++ mysql-read-replica/install-mysql.sh
@@ -1,4 +1,4 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 # update OS
 sudo yum update -y 
@@ -7,9 +7,9 @@ sudo yum update -y
 wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm --no-check-certificate
 sudo rpm -ivh mysql57-community-release-el7-11.noarch.rpm
 
-sudo yum update
+sudo yum update -y
 
-sudo yum install mysql-server
+sudo yum install mysql-server -y
 
 
 #wget https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl
