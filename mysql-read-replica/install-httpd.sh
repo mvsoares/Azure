4c62964a686310c939fb27575b00042fc1dcc21b
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-25 07:51:12 -0300

    .

--- mysql-read-replica/install-httpd.sh
+++ mysql-read-replica/install-httpd.sh
@@ -1,14 +1,15 @@
-yum install -y nmap-ncat httpd
+yum install -y httpd
 
 systemctl enable httpd
+
 
 echo '#!/bin/bash
 echo "Content-Type: text/plain"
 echo
 echo This is a sample
-date' > /var/www/cgi-bin/test.cgi
+date' > /var/www/cgi-bin/probe.cgi
 
-chmod +x /var/www/cgi-bin/test.cgi
+chmod +x /var/www/cgi-bin/probe.cgi
 
 systemctl restart httpd 
 
