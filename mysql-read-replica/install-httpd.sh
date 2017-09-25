74697683bd75affe2624fcfe1f945be19ccce42f
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-25 08:29:09 -0300

    .

--- mysql-read-replica/install-httpd.sh
+++ mysql-read-replica/install-httpd.sh
@@ -2,12 +2,26 @@ yum install -y httpd
 
 systemctl enable httpd
 
+wget --no-check-certificate -O /var/www/cgi-bin/probe.sh https://raw.githubusercontent.com/mvsoares/Azure/master/mysql-read-replica/probe.sh 
+wget --no-check-certificate -O /var/www/cgi-bin/mysql-probe https://raw.githubusercontent.com/mvsoares/Azure/master/mysql-read-replica/mysql-probe 
+chmod +x /var/www/cgi-bin/probe.sh
+chmod +x /var/www/cgi-bin/mysql-probe
 
 echo '#!/bin/bash
-echo "Content-Type: text/plain"
-echo
-echo This is a sample
-date' > /var/www/cgi-bin/probe.cgi
+probeResult=$(bash /var/www/cgi-bin/probe.sh)
+
+if [ $probeResult -eq 0 ] ; then
+	echo "Content-Type: text/plain"
+	echo
+	echo This is a sample
+	date' > /var/www/cgi-bin/probe.cgi
+else
+	echo "Content-Type: text/plain"
+	echo
+	echo This is a sample
+	date' > /var/www/cgi-bin/probe.cgi
+fi
+
 
 chmod +x /var/www/cgi-bin/probe.cgi
 
