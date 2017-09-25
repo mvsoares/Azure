037fef25aaf1c91d04f393265b4690c0aab9ece0
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-25 10:52:17 -0300

    .

--- mysql-read-replica/install-httpd.sh
+++ mysql-read-replica/install-httpd.sh
@@ -1,11 +1,15 @@
 yum install -y httpd
 
+set enforce 0 
+sed -i -e s/"SELINUX=enforcing"/"SELINUX=disabled"/g /etc/selinux/config
+
 systemctl enable httpd
 
 rm -f /var/www/cgi-bin/mysql-probe
 
 wget --no-check-certificate -O /var/www/cgi-bin/mysql-probe https://raw.githubusercontent.com/mvsoares/Azure/master/mysql-read-replica/mysql-probe 
 chmod +x /var/www/cgi-bin/mysql-probe
+chown apache.apache /var/www/cgi-bin/mysql-probe
 
 systemctl restart httpd 
 
