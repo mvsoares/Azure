cbc4b52a941ccd727c38e55292129a1bbe9b6074
Marcus Vinicius Soares <masoar@microsoft.com>
2017-09-25 09:49:50 -0300

    .

--- mysql-read-replica/install-httpd.sh
+++ mysql-read-replica/install-httpd.sh
@@ -2,12 +2,9 @@ yum install -y httpd
 
 systemctl enable httpd
 
-rm -f /var/www/cgi-bin/probe.sh
 rm -f /var/www/cgi-bin/mysql-probe
 
-wget --no-check-certificate -O /var/www/cgi-bin/probe.sh https://raw.githubusercontent.com/mvsoares/Azure/master/mysql-read-replica/probe.sh 
 wget --no-check-certificate -O /var/www/cgi-bin/mysql-probe https://raw.githubusercontent.com/mvsoares/Azure/master/mysql-read-replica/mysql-probe 
-chmod +x /var/www/cgi-bin/probe.sh
 chmod +x /var/www/cgi-bin/mysql-probe
 
 systemctl restart httpd 
