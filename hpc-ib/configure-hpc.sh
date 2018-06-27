f95675d1edb967f32246c8a68888e06b481aa21f
Marcus Vinícius Soares <marcus.soares@microsoft.com>
2018-06-27 09:17:26 -0300

    .

--- hpc-ib/configure-hpc.sh
+++ hpc-ib/configure-hpc.sh
@@ -1,23 +1,5 @@
-cat > /etc/yum.repos.d/elrepo.repo <<EOF
-[elrepo]
-name=ELRepo.org Community Enterprise Linux Repository - el7
-baseurl=http://elrepo.org/linux/elrepo/el7/$basearch/
-        http://mirrors.coreix.net/elrepo/elrepo/el7/$basearch/
-        http://mirror.rackspace.com/elrepo/elrepo/el7/$basearch/
-        http://repos.lax-noc.com/elrepo/elrepo/el7/$basearch/
-        http://mirror.ventraip.net.au/elrepo/elrepo/el7/$basearch/
-mirrorlist=http://mirrors.elrepo.org/mirrors-elrepo.el7
-enabled=1
-gpgcheck=0
-
-[epel]
-name=Extra Packages for Enterprise Linux 7 - $basearch
-#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch
-metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch
-failovermethod=priority
-enabled=1
-gpgcheck=0
-EOF
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
 
 yum install  mpich-3.0 mpich-3.0-devel htop iftop -y
 
