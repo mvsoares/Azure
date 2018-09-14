ba2608ebbdc6ef39f43022fda07af64b3381be61
Marcus Soares <marcus.soares@gmail.com>
2018-09-14 15:59:30 -0300

    Extensions add

---
+++ enable-docker-scripts/install-centos.sh
@@ -0,0 +1,7 @@
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl status docker
+sudo systemctl enable docker
+sudo usermod -aG docker azuretest
+
