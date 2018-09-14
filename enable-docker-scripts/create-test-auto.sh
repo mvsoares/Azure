fe56cc9e4cb868230f6fc6d8dfa086c3e711ed3a
Marcus Vinícius Soares <marcus.soares@microsoft.com>
2018-09-14 16:48:48 -0300

    Bastion first , comments

--- enable-docker-scripts/create-test-auto.sh
+++ enable-docker-scripts/create-test-auto.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+
+# variable defintion 
 baseName=testnw
 rg=rg-$baseName
 vnet=vnet-$baseName
@@ -9,6 +11,7 @@ adminUsername=azuretest
 adminPassword=L8ytNthsJDk4fQB
 location=southcentralus
 
+# create group / network / nsg 
 az group create -n $rg  -l $location 
 
 az network vnet create -g $rg \
@@ -30,6 +33,39 @@ az network nsg rule create --resource-group  $rg \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 80 \
   --access allow
 
+## bastion server 
+vmPrefix=bastion1
+imageName=OpenLogic:CentOS:7.5:7.5.20180815
+vmSize=Standard_D4s_v3
+baseNameBastion=$vmPrefix
+
+az network public-ip create --resource-group $rg \
+    --name pip-$baseNameBastion --allocation-method static --idle-timeout 4
+
+az network nic create \
+    -n nic-$baseNameBastion \
+    -g $rg \
+    --subnet $subnet \
+    --network-security-group $nsgName \
+    --public-ip-address pip-$baseNameBastion \
+    --vnet-name $vnet 
+
+az vm create \
+    -g $rg \
+    --os-disk-name osdisk-$baseNameBastion \
+    --name vm-$baseNameBastion \
+    --nics nic-$baseNameBastion \
+    --storage-sku premium_lrs \
+    --size $vmSize \
+    --image $imageName \
+    --admin-username $adminUsername \
+    --admin-password $adminPassword \
+    --authentication-type password
+
+az vm extension set --resource-group $rg --vm-name vm-$baseNameBastion --name customScript --publisher Microsoft.Azure.Extensions --settings '{"fileUris": ["https://raw.githubusercontent.com/mvsoares/Azure/master/enable-docker-scripts/install-centos.sh"],"commandToExecute": "bash install-centos.sh"}'
+
+
+# Ubuntu ACC
 vmPrefix=ubuntu-acc
 accNet=true
 imageName=UbuntuLTS
@@ -61,7 +97,7 @@ do
     az vm extension set -g $rg --vm-name vm-$baseNameLoop --name customScript --publisher Microsoft.Azure.Extensions --settings '{"fileUris": ["https://raw.githubusercontent.com/mvsoares/Azure/master/enable-docker-scripts/install-ubuntu.sh"],"commandToExecute": "bash install-ubuntu.sh"}'
 done
 
-
+# Ubuntu NON-ACC
 vmPrefix=ubuntu-noacc
 accNet=false
 imageName=UbuntuLTS
@@ -94,6 +130,7 @@ do
 
 done
 
+# centos ACC
 vmPrefix=centos-acc
 accNet=true
 imageName=OpenLogic:CentOS:7.5:7.5.20180815
@@ -125,6 +162,7 @@ do
     az vm extension set --resource-group $rg --vm-name vm-$baseNameLoop --name customScript --publisher Microsoft.Azure.Extensions --settings '{"fileUris": ["https://raw.githubusercontent.com/mvsoares/Azure/master/enable-docker-scripts/install-centos.sh"],"commandToExecute": "bash install-centos.sh"}'
 done
 
+# centos NON-ACC
 vmPrefix=centos-noacc
 accNet=false
 imageName=OpenLogic:CentOS:7.5:7.5.20180815
@@ -155,33 +193,3 @@ do
 
     az vm extension set --resource-group $rg --vm-name vm-$baseNameLoop --name customScript --publisher Microsoft.Azure.Extensions --settings '{"fileUris": ["https://raw.githubusercontent.com/mvsoares/Azure/master/enable-docker-scripts/install-centos.sh"],"commandToExecute": "bash install-centos.sh"}'
 done
-
-vmPrefix=bastion1
-imageName=OpenLogic:CentOS:7.5:7.5.20180815
-vmSize=Standard_D4s_v3
-baseNameBastion=$vmPrefix
-
-az network public-ip create --resource-group $rg \
-    --name pip-$baseNameBastion --allocation-method static --idle-timeout 4
-
-az network nic create \
-    -n nic-$baseNameBastion \
-    -g $rg \
-    --subnet $subnet \
-    --network-security-group $nsgName \
-    --public-ip-address pip-$baseNameBastion \
-    --vnet-name $vnet 
-
-az vm create \
-    -g $rg \
-    --os-disk-name osdisk-$baseNameBastion \
-    --name vm-$baseNameBastion \
-    --nics nic-$baseNameBastion \
-    --storage-sku premium_lrs \
-    --size $vmSize \
-    --image $imageName \
-    --admin-username $adminUsername \
-    --admin-password $adminPassword \
-    --authentication-type password
-
-az vm extension set --resource-group $rg --vm-name vm-$baseNameBastion --name customScript --publisher Microsoft.Azure.Extensions --settings '{"fileUris": ["https://raw.githubusercontent.com/mvsoares/Azure/master/enable-docker-scripts/install-centos.sh"],"commandToExecute": "bash install-centos.sh"}'
