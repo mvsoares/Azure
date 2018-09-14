05c789b819303c4e6e7bc252664d87e28aac4de3
Marcus Vinicius Soares <marcus.soares@gmail.com>
2018-09-14 16:30:31 -0300

    Start nginx container

--- enable-docker-scripts/install-centos.sh
+++ enable-docker-scripts/install-centos.sh
@@ -1,7 +1,8 @@
 sudo yum check-update
+sudo yum install sysstat -y
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl status docker
 sudo systemctl enable docker
 sudo usermod -aG docker azuretest
-
+docker run -d -p 80:80 mvsoares/nginx-centos:v1
