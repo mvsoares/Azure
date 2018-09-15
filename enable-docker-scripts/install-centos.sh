6d0ac6d5e22f83f1be83f6152425014646bee830
Marcus Vinicius Soares <marcus.soares@microsoft.com>
2018-09-14 22:34:09 -0300

    fix

--- enable-docker-scripts/install-centos.sh
+++ enable-docker-scripts/install-centos.sh
@@ -1,6 +1,5 @@
-sudo yum check-update
-sudo yum install sysstat epel-release -y
 sudo yum update -y
+sudo yum install sysstat epel-release -y
 sudo yum install siege -y
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
